06955989bff972ba474c27d246658a291dfe43ff
Alex <alexzalox@gmail.com>
2022-10-31 18:13:39 +0800

    新增現在時間

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## 幸運之星請來電！超Lucky手機靈數
-- 實作一個測手機靈數網頁
+- 卜算手機靈數網頁
 - 參考網頁： https://mbstar.pixnet.net/blog/post/27989163-%e5%b9%b8%e9%81%8b%e4%b9%8b%e6%98%9f%e8%ab%8b%e4%be%86%e9%9b%bb%ef%bc%81%e8%b6%85lucky%e6%89%8b%e6%a9%9f%e9%9d%88%e6%95%b8?fbclid=IwAR2xcoBp6gs6b7UV1aRD4wdCb7A8C1E5PVMMuUxcLD0bvNc62J9UHN3guX0
--- js/index.js
+++ js/index.js
@@ -16,3 +16,13 @@ function showDivs(n) {
     }
     x[slideIndex - 1].style.display = "block";
 }
+
+// 現在時間
+const timeEl = document.querySelector("#date");
+function getTime() {
+    let date = new Date();
+    timeEl.innerText = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")} ${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
+
+    setTimeout(getTime, 1000);
+
+}
